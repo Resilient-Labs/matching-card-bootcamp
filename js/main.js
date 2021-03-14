1830cde3414c17de193db38c15ec8db477b02347
Juan Vistro <jpmbvistro@gmail.com>
2021-03-14 08:25:02 -0400

    Adding multi card and difficulty functionality

--- js/main.js
+++ js/main.js
@@ -28,10 +28,14 @@ class FlipCardGame{
     this.matchAmount = matchAmount
     this.deck = []
     this.isOver = false
+
+    //Clear Board
+    this.htmlElement.innerHTML = ''
+
     //Create Deck of cards
     for(let i = 0 ; i< cardFaces.length ; i++){
       for(let j = 0 ; j < matchAmount ; j++){
-        this.deck.push(new Card(cardFaces[i][0], cardFaces[1], -1))
+        this.deck.push(new Card(cardFaces[i][0], cardFaces[i][1], -1))
       }
     }
     //Initialize DOM Interface
@@ -97,7 +101,6 @@ class FlipCardGame{
       this.deck[currentIndex] = this.deck[randomIndex];
       this.deck[randomIndex] = temporaryValue;
     }
-    console.log(this.deck)
   }
 
   /**********
@@ -112,10 +115,16 @@ class FlipCardGame{
     domCardList.forEach((item, i) => {
       let card = this.deck[i]
       card.domIndex = item.id
+      let img = item.querySelector('img')
+      let span = item.querySelector('span')
+      img.classList.add('hidden')
+      span.classList.add('hidden')
       if(card.isImg){
-        item.querySelector('img').src = card.value
+        img.src = card.value
+        img.classList.remove('hidden')
       } else {
-        item.querySelector('span').innerText = card.value
+        span.innerText = card.value
+        span.classList.remove('hidden')
       }
     });
   }
@@ -213,13 +222,81 @@ match number: number of card duplicates to match
 ***********************************/
 class Player{
   constructor(domElement){
-    let customCards = [['img/cards/screenshots/news.png', true],['img/cards/screenshots/nyt.png', true],['img/cards/screenshots/portfoliov2.png', true],['img/cards/screenshots/simple-calc.png', true],['img/cards/screenshots/slot-machine.png', true],['img/cards/screenshots/tictactoebeans.png', true],['img/cards/screenshots/to-do-list.png', true],['img/cards/screenshots/true-color.png', true],['img/cards/screenshots/yelp-screenshot.png', true]]
-    this.game = new FlipCardGame(domElement, 2, customCards)
+    this.baseCards = [[1, false], [2, false], [3, false], [4, false], [5, false]]
+    this.customCards = [['img/cards/screenshots/news.png', true],['img/cards/screenshots/nyt.png', true],['img/cards/screenshots/portfoliov2.png', true],['img/cards/screenshots/simple-calc.png', true],['img/cards/screenshots/slot-machine.png', true],['img/cards/screenshots/tictactoebeans.png', true],['img/cards/screenshots/to-do-list.png', true],['img/cards/screenshots/true-color.png', true],['img/cards/screenshots/yelp-screenshot.png', true]]
+    this.domElement = domElement
+    this.game = null
+    // let type = [0,1,2]
+    this.matchAmount = 2
+    this.currentCards = null
+    this.playBaseGame()
+  }
+  playBaseGame(matchAmount=this.matchAmount){
+    this.matchAmount = matchAmount
+    this.currentCards = this.baseCards
+    this.game = new FlipCardGame(this.domElement, matchAmount, this.baseCards)
+  }
+  playCustomGame(matchAmount=this.matchAmount){
+    this.matchAmount = matchAmount
+    this.currentCards = this.customCards
+    this.game = new FlipCardGame(this.domElement, matchAmount, this.customCards)
+  }
+  playMixGame(matchAmount=this.matchAmount){
+    this.matchAmount = matchAmount
+    this.currentCards = this.baseCards.concat(this.customCards)
+    this.game = new FlipCardGame(this.domElement, matchAmount, this.baseCards.concat(this.customCards))
+  }
+  resetDifficulty(matchAmount=this.matchAmount){
+    this.matchAmount = matchAmount
+    this.game = new FlipCardGame(this.domElement, matchAmount, this.currentCards)
   }
 }
 
+
+//Menu Controller
 let initialize = function(){
   let player1 = new Player(document.querySelector('#gameboard'))
+  let difficultyButtons = document.querySelectorAll('.difficulty button')
+  let cardTypeButtons = document.querySelectorAll('.card-type button')
+  document.querySelector('#reset').addEventListener('click', reset)
+  difficultyButtons.forEach((item, i) => {
+    item.addEventListener('click',changeDifficulty)
+  });
+  cardTypeButtons.forEach((itemz, j) => {
+    itemz.addEventListener('click', changeCards)
+  });
+  // player1.playBaseGame()
+
+
+  function reset(element){
+    player1.resetDifficulty()
+  }
+
+  function changeDifficulty(element){
+    difficultyButtons.forEach((item, i) => {
+      item.classList.remove('selected')
+    });
+    element.currentTarget.classList.add('selected')
+    let difficulty = -1
+    if(element.currentTarget.id === 'hard'){
+      difficulty = 4
+    }
+    else if (element.currentTarget.id === 'medium') {difficulty =3}
+    else { difficulty = 2}
+    player1.resetDifficulty(difficulty)
+  }
+
+  function changeCards(element){
+    console.log('changing cards')
+    cardTypeButtons.forEach((item, i) => {
+      item.classList.remove('selected')
+    });
+    element.currentTarget.classList.add('selected')
+    if(element.currentTarget.id==='mix'){player1.playMixGame()}
+    else if(element.currentTarget.id==='portfolio'){player1.playCustomGame()}
+    else if(element.currentTarget.id==='numbers'){player1.playBaseGame()}
+  }
+
 }
 
 initialize()
