cedf9216d0c739bf72a81f5ceca0baacde1d0193
Abdullahi27 <111465917+Abdullahi27@users.noreply.github.com>
2022-10-22 20:59:34 -0400

    updated

--- main.js
+++ main.js
@@ -1,112 +1,61 @@
 let myCardList = [
-    'images/benzema.png',
-    'images/cr7.png',
-    'images/deBruyne.png',
-    'images/kante.png',
-    'images/lewy.png',
-    'images/mane.png',
-    'images/messi.png',
-    'images/neymar.png',
-    'images/salah.png',
-    'images/son.png',
+    'images/benzema.png','images/cr7.png','images/deBruyne.png','images/kante.png','images/lewy.png',
+    'images/mane.png','images/messi.png','images/neymar.png','images/salah.png','images/son.png',
 ]
 
-let audio = document.getElementById("song");
-audio.volume = 0.5;
-
-let myCards
-let gameBoard = []
-
-function shuffle(){
-    myCards = myCardList.concat(myCardList); 
-    for (let i=0; i < myCards.length; i++ ){
-        let j = Math.floor(Math.random() * 20);
-    
-    let swap = myCards[i] 
-    myCards[i]= myCards[j];
-    myCards[j]= swap;
-    }
-}
-shuffle()
-
-let rows = 4
-let columns = 5
-
-function playGame (){
-    let cardId 
-    for (let a = 0; a < rows; a++){
-        let row = [];
-        for (let b = 0; b < columns; b++){
-            let cardImg = myCards.pop();
-            cardId = a.toString() + '-' + b.toString()
-            row.push(cardImg); 
-            document.getElementById('gameBoard').innerHTML += `
-            <img src="${cardImg}" class="card" id="${cardId}">`
-        }
-        gameBoard.push(row)
-    }
-
-setTimeout(hide, 1000)
-}
-playGame()
-
-function hide() {
-    for (let a = 0; a < rows; a++){
-        for (let b = 0; b < columns; b++) {
-            let backCard = document.getElementById(a.toString() + '-' + b.toString());
-            backCard.src = 'images/back.png'
-        }
-    }
+function getShuffledDeck(){
+    return myCardList.concat(myCardList).map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort).map(({ value }) => value)
 }
 
-document.querySelectorAll('img').forEach(item => item.addEventListener('click', selectedCards))
 
-let count = 0
-let errors = 0 
-let img1id
-let img2id
-let img1src
-let img2src
-let setTimeoutFinished = true
 
-function selectedCards(e) {
-    if (setTimeoutFinished){
-    let splitter = e.target.id.split('-')
-    if (count === 0){
-        e.target.src = gameBoard[+splitter[0]][+splitter[1]]
-        img1id = e.target.id
-        img1src = e.target.src
-        count++
-        return
-   }
+function setUpBoardandDisplayCards(){
+    myCardList = getShuffledDeck(myCardList)
+    myCardList.forEach((item, index) => { //displays every image along with the index as ID
+        document.getElementById('gameBoard').innerHTML += ` 
+        <img src="${item}" class="card" id="${index}">`
+        setTimeout(()=>{
+            myCardList.forEach((_, index) => { 
+                let img = document.getElementById(`${index}`)
+                img.addEventListener('click', handleClick)
+                img.src = "images/back.png"
+         }) },1000);
+         })
+    }
 
-   if (count === 1){
-        e.target.src = gameBoard[+splitter[0]][+splitter[1]]
-        img2id = e.target.id
-        img2src = e.target.src
+setUpBoardandDisplayCards()
 
-        if (img1src === img2src) {
-            document.getElementById(`${img1id}`).removeEventListener('click', selectedCards)
-            document.getElementById(`${img2id}`).removeEventListener('click', selectedCards)
+let clickedTwoImages = [] , errors = 0 , setTimeoutFinished = true
+
+function handleClick(event){
+    if(setTimeoutFinished && clickedTwoImages[0] !== event.target.id){
+    event.target.src = myCardList[event.target.id]
+    clickedTwoImages.push(event.target.id)
+    if(clickedTwoImages.length ===2){
+        if(myCardList[clickedTwoImages[0]] === myCardList[clickedTwoImages[1]]){
+            document.getElementById(`${clickedTwoImages[0]}`).removeEventListener('click', handleClick)
+            document.getElementById(`${clickedTwoImages[1]}`).removeEventListener('click', handleClick)
             let audio = new Audio('goal.mp3');
             audio.load();
-            audio.play(); 
-        }  
-
-        else {
+            audio.play();  // Play goal sound
+            clickedTwoImages = []
+        }else{
             setTimeoutFinished = false 
             setTimeout(() => {
-                document.getElementById(`${img1id}`).src = 'images/back.png'
-                document.getElementById(`${img2id}`).src = 'images/back.png'
+                document.getElementById(`${clickedTwoImages[0]}`).src = 'images/back.png'
+                document.getElementById(`${clickedTwoImages[1]}`).src = 'images/back.png'
                 errors += 1;
                 document.getElementById('errors').innerHTML = errors;
             setTimeoutFinished = true
+            clickedTwoImages = []
             }, 1000)
         }
-        count = 0 
     }
-  }
-}
+}}
+
+let audio = document.getElementById("song");
+audio.volume = 0.5;
 
 
 
