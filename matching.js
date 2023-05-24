2c1acf71cab49a425a0cd55d70633f8512bdfe2b
Khadir | FsDevKD <125905976+FSDevKD@users.noreply.github.com>
2023-05-24 10:07:00 -0400

    pushing

--- matching.js
+++ matching.js
@@ -1,127 +1,108 @@
-//Variables
+// Variables
 const game = document.querySelector('.game-container');
 let firstCard = null;   // first click
 let secondCard = null;  // second click
 let isFlipped = false;  // checking for flip state
+let canFlip = true;     // allow card flipping
 
-//Shuffle array 
+// Shuffle array 
 function shuffle(array) {
+    let current = array.length; 
 
-    let current = array,length; 
-
-    while(current !== 0){   
-
+    while (current !== 0) {   
         const random = Math.floor(Math.random() * current);
-
         current--;
-
         [array[current], array[random]] = [array[random], array[current]];
     }
 
     return array;
 }
 
-//Generate cards
+// Generate cards
 function generateCards() {
-
     const cards = [];
 
-    for( let i = 0; i < 10; i++ ) {
-
-        cards.push(i)
-
-        cards.push(i)
+    for (let i = 1; i <= 5; i++) {
+        cards.push(i);
+        cards.push(i);
     }
 
     return shuffle(cards);
-
 }
 
-//create a card element
-function createCard(value){
-
+// Create a card element
+function createCard(value) {
     const card = document.createElement('div');
-
     card.classList.add('card');
-
     card.dataset.value = value;
+    
+    const front = document.createElement('div');
+    front.classList.add('card-front');
+    const frontImage = document.createElement('img');
+    frontImage.src = `${value}.png`;
+    frontImage.alt = `Card ${value}`;
+    front.appendChild(frontImage);
 
-    card.textContent = value;
+    const back = document.createElement('div');
+    back.classList.add('card-back');
+    const backImage = document.createElement('img');
+    backImage.src = 'back.png';
+    backImage.alt = 'Card Back';
+    back.appendChild(backImage);
 
-    card.addEventListener('click',flip)
-
-    game.appendChild(card);
-
+    card.appendChild(front);
+    card.appendChild(back);
+    card.addEventListener('click', flip);
+    game.appendChild(card)
 }
 
-//flip a card
+// Flip a card
 function flip(event) {
-
-    if(isFlipped) return;
+    if (!canFlip) return;
     
-    const current = event.target;
+    const current = event.currentTarget;
 
+    if (!current.classList.contains('card') || current.classList.contains('flip')) return;
+    
     current.classList.add('flip');
 
-    if(!firstCard){
-
+    if (!firstCard) {
         firstCard = current;
-
     } else {
-
         secondCard = current;
-
-        checkMatch()
-
+        checkMatch();
     }
-
 }
 
-//check if cards match
+// Check if cards match
 function checkMatch() {
+    canFlip = false;
 
-    isFlipped = true;
-
-    if(firstCard.dataset.value === secondCard.dataset.value) {
-
+    if (firstCard.dataset.value === secondCard.dataset.value) {
         setTimeout(() => {
-
-            firstCard.removeEventListener('click',flip);
-
-            secondCard.removeEventListener('click',flip);
-
+            firstCard.removeEventListener('click', flip);
+            secondCard.removeEventListener('click', flip);
             firstCard = null;
-
             secondCard = null;
-
-            isFlipped = false;
-
+            canFlip = true;
         }, 1000);
-
     } else {
-
         setTimeout(() => {
-            
             firstCard.classList.remove('flip');
-
             secondCard.classList.remove('flip');
-
             firstCard = null;
-
             secondCard = null;
-
-            isFlipped = false;
-
-        }, 1000);
+            canFlip = true;
+        }, 2000);
     }
 }
-//reset new game
+
+// Reset new game
 function reset() {
-
     const deck = generateCards();
-
-    deck.forEach( value => createCard(value));
+    game.innerHTML = '';
+    deck.forEach((value) => createCard(value));
 }
-//start game
 
+// Start game
 reset();
