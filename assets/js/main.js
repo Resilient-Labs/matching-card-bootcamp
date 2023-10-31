9e8920fd6e74f52465766fe197b5d5cc4ad818da
socratic617 <erik.solis02135@gmail.com>
2023-10-31 01:17:13 -0400

    working game need to reset cards and fix imgs

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,12 @@ document.querySelector("#startGame").addEventListener("click", playGame);
 
 let cards = [];
 
-let frontCard, secondCard;
+let firstCard, secondCard;
 let scoreTotal = 0;
+
+let totalMoves = 0;
+
+let activeGame = true;
 
 function playGame() {
   fetch("./assets/cards.json")
@@ -15,13 +19,19 @@ function playGame() {
       // found the randomizer for cards .sort... on stack overflow: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
       cards = [...data, ...data].sort(() => 0.5 - Math.random());
 
+      totalMoves = data.length;
+
       let clickableCards = document.querySelectorAll(".cards");
 
-      clickableCards.forEach((card) => {
+      for( let i = 0; i < cards.length; i++){
+        let card = clickableCards[i];
         card.addEventListener("click", selectedCard);
-        let cardFront = card.querySelector(".card-img-front");
 
-      });
+        let cardFront = card.querySelector(".card-img-front")
+        cardFront.id = cards[i].name
+        let cardImg = cardFront.querySelector(".card-img");
+        cardImg.src = cards[i].image;
+      }
 
       console.log(cards);
     });
@@ -29,6 +39,54 @@ function playGame() {
 
 }
 
-function selectedCard() {
-  console.log("hi oni");
+function selectedCard(e) {
+
+  console.log('INSELECTED CARDS')
+
+  if (totalMoves == 0 && !activeGame) {
+    console.log("totalMoves == 0");
+    return;
+  }
+
+  //Check that front card is not set
+  if(firstCard === undefined){
+    console.log("firstCard === undefined");
+    firstCard = e.currentTarget;
+    firstCard.id = "flip"
+  }
+  else if (secondCard == undefined){
+    console.log(" else LINE 58");
+    secondCard = e.currentTarget;
+    secondCard.id = "flip";
+    if ( secondCard.querySelector(".card-img-front").id == firstCard.querySelector(".card-img-front").id) {
+      console.log(" first card = second card");
+      firstCard = undefined;
+      secondCard = undefined;
+      totalMoves--;
+
+      if(totalMoves == 0){
+        document.querySelector("#winner").innerText = "Looney Baby Wins, Thats all Folks!";
+        scoreTotal++;
+        document.querySelector(".score").innerText = scoreTotal;
+        console.log("moves left: " + totalMoves);
+      }
+    }
+    else{
+      activeGame = false;
+      console.log(" first card NOT EQUAL second card");
+      setTimeout(()=> {
+        firstCard.id = "";
+        secondCard.id = "";
+        firstCard = undefined;
+        secondCard = undefined;
+        activeGame = true;
+      }, 1000);
+
+    }
+  }
+
+
+  console.log("event: ");
+  console.log(e.currentTarget.querySelector(".card-img-front"));
+  console.log(e);
 }
